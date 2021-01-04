6edf02f9b719ae16c163070b29fb1ade54c08b1d
rejuvyesh <mail@rejuvyesh.com>
2021-01-04 20:52:45 +0000

    add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using MultiAgentPOMDPs
+using POMDPs
 using Test
 
 @testset "MultiAgentPOMDPs.jl" begin
-    # Write your tests here.
+    @testset "inference" begin
+        mutable struct X <: JointMDP{Vector{Float64},Vector{Bool}} end
+        abstract type Z <: JointMDP{Vector{Float64},Vector{Int}} end
+        mutable struct Y <: Z end
+
+        @test_throws ErrorException statetype(Int)
+        @test_throws ErrorException actiontype(Int)
+        @test_throws ErrorException obstype(Int)
+
+        @test statetype(X) == Vector{Float64}
+        @test statetype(Y) == Vector{Float64}
+        @test actiontype(X) == Vector{Bool}
+        @test actiontype(Y) == Vector{Int}
+    end
 end
