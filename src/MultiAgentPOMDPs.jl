f84455380d9402de048944acabc0d3d0dc98e8e3
rejuvyesh <mail@rejuvyesh.com>
2021-01-04 21:19:40 +0000

    doc update

--- src/MultiAgentPOMDPs.jl
+++ src/MultiAgentPOMDPs.jl
@@ -12,7 +12,7 @@ Return the number of agents in the (PO)MDP
 function n_agents end
 
 """
-    function agent_actions (m::JointMDP, idx::Int64, s::S) where S
+    function agent_actions(m::JointMDP, idx::Int64, s::S) where S
 Returns the discrete actions for the given agent index.
 
 !!! note
@@ -21,17 +21,26 @@ Returns the discrete actions for the given agent index.
 function agent_actions end
 
 """
-    function agent_states (m::JointMDP, idx::Int64)
+    function agent_states(m::JointMDP, idx::Int64)
 Returns the discrete states for the given agent index
 """
 function agent_states end
 
+"""
+    function agent_actionindex(m::JointMDP, idx::Int64, a::A) where A
+Returns the integer index of action `a` for agent `idx`. Used for discrete models only.
+"""
 function agent_actionindex end
 
+"""
+    function agent_actionindex(m::JointMDP, idx::Int64, s::S) where S
+Returns the integer index of state `s` for agent `idx`. Used for discrete models only.
+"""
 function agent_stateindex end
 
 """
     function coordination_graph(m::JointMDP)
+    function coordination_graph(m::JointMDP, s::S) where S
 Returns the LightGraphs.SimpleGraph (or any appropriate structure) for the coordination graph.
 """
 function coordination_graph end
